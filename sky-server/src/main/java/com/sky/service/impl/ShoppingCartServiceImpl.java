bd4ee615981b7e7f2ba5451f1155ca8846e18674
Lukaka <wcf179080890@gmail.com>
2023-11-29 16:03:36 +0800

    清空购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -71,4 +71,10 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
         return list;
     }
+
+    @Override
+    public void clearAll() {
+        Long userId = BaseContext.getCurrentId();
+        shoppingCartMapper.clearAll(userId);
+    }
 }
